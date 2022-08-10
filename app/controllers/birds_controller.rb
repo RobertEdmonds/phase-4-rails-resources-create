3ae76813f64c506aca8284eede4c3629299e89a0
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-08-10 14:29:30 -0400

    commit tree f89c46807b0590089f764c9f9fdcc7f21f44816e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +14,17 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
